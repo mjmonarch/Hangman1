c47d59967a2a0da0eb0ce599020c1453b85aa1a3
Sergii Kamenskyi <serg@apple.com>
2020-07-13 20:37:59 +0300

    stage 7 completed

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -129,7 +129,9 @@
 # else:
 #     print("You are hanged!")
 
-# ---------------------------7---------------------------
+# ---------------------------8---------------------------
+#
+
 import random
 
 
@@ -161,37 +163,48 @@ words = ['python', 'java', 'kotlin', 'javascript']
 global word_needed
 global letters_chosen
 global letters_to_guess
-letters_chosen = set()
-word_needed = random.choice(words)
-letters_to_guess = len(set(word_needed))
-hidden_word = list('-' * len(word_needed))
 
 print("H A N G M A N")
+while True:
+    letters_chosen = set()
+    word_needed = random.choice(words)
+    letters_to_guess = len(set(word_needed))
+    hidden_word = list('-' * len(word_needed))
 
-tries = 8
-while tries > 0:
-    print()
-    print("".join(hidden_word))
-    print("Input a letter: >")
-    letter = input()
-    result = check_letter(letter)
-    if result == -4:
-        print("You should input a single letter")
-    elif result == -3:
-        print("It is not an ASCII lowercase letter")
-    elif result == -2:
-        print("You already typed this letter")
-    elif result == -1:
-        tries -= 1
-        print("No such letter in the word")
-    else:
-        for j in result:
-            hidden_word[j] = letter
-    if letters_to_guess == 0:
+    print('Type "play" to play the game, "exit" to quit:')
+    choice = input()
+
+    if choice == "exit":
+        break
+    if choice != "play":
+        continue
+
+    tries = 8
+    while tries > 0:
         print()
         print("".join(hidden_word))
-        print("""You guessed the word!
-You survived!""")
-        break
-else:
-    print("You are hanged!")
+        print("Input a letter: >")
+        letter = input()
+        result = check_letter(letter)
+        if result == -4:
+            print("You should input a single letter")
+        elif result == -3:
+            print("It is not an ASCII lowercase letter")
+        elif result == -2:
+            print("You already typed this letter")
+        elif result == -1:
+            tries -= 1
+            print("No such letter in the word")
+        else:
+            for j in result:
+                hidden_word[j] = letter
+        if letters_to_guess == 0:
+            print()
+            print("".join(hidden_word))
+            print("""You guessed the word!
+    You survived!
+    """)
+            break
+    else:
+        print("You are hanged!")
+        print()
